0d6f440b7538c85b94ab7fc90dabba40a7d14b6e
LKV <a23202418@gmail.com>
2021-12-06 15:09:58 +0800

    refresh

--- X.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    printf("hello\n");
-    system("pause");
-    return 0;
-}
--- zerojudge/scanf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    printf("%d\n",EOF);
-
-    system("pause");
-    return 0;
-}
