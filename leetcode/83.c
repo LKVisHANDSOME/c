d87fcd93592330fc4aa6093709c215711e65773d
LKV <a23202418@gmail.com>
2021-12-13 22:06:40 +0800

    83

--- leetcode/83.c
+++ leetcode/83.c
@@ -6,17 +6,19 @@
  * };
  */
 
-
-struct ListNode* deleteDuplicates(struct ListNode* head){
-    if(!head)   return head;
-    Node *p=head,*tail=head;
-    while(p!=NULL){
-        if(p->val!=tail->val){
-            tail->next=p;
-            tail=tail->next;
+struct ListNode *deleteDuplicates(struct ListNode *head)
+{
+    if (!head)  return head;
+    Node *p = head, *tail = head;
+    while (p != NULL)
+    {
+        if (p->val != tail->val)
+        {
+            tail->next = p;
+            tail = tail->next;
         }
-        p=p->next;
+        p = p->next;
     }
-    tail->next=NULL;
+    tail->next = NULL;
     return head;
 }
